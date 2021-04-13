c9ceab2b4be4352d853ae671b45792a66e211e64
KaneTimlin <ktimlin@montgomerycollege.edu>
2021-04-12 21:00:46 -0400

    assignment 5 - javadoc

--- Assignment 5/src/MorseCodeConverter.java
+++ Assignment 5/src/MorseCodeConverter.java
@@ -2,8 +2,18 @@ import javax.crypto.AEADBadTagException;
 import java.io.*;
 import java.util.ArrayList;
 
-public class MorseCodeConverter {
+/**
+ * @author Kane Timlin
+ */
 
+public class MorseCodeConverter {
+    /**
+     * This method accepts a file containing as many lines as needed of morse code and translates it into english
+     * It uses the other version of itself to accomplish this
+     * @param file the file containing the morse code
+     * @return the translated text
+     * @throws FileNotFoundException if the file cannot be thrown
+     */
     public static String convertToEnglish(File file) throws FileNotFoundException {
         StringBuilder str = new StringBuilder();
         String code;
@@ -21,6 +31,11 @@ public class MorseCodeConverter {
         return convertToEnglish(code);
     }
 
+    /**
+     * This method accepts a string of morse code and turns it into a string of english text
+     * @param code the morse code to be translated
+     * @return the english text
+     */
     public static String convertToEnglish(String code) {
         StringBuilder output = new StringBuilder();
         MorseCodeTree<String> tree = new MorseCodeTree<>();
@@ -36,6 +51,10 @@ public class MorseCodeConverter {
         return output.substring(1);
     }
 
+    /**
+     * This method turns the morse code tree into an arraylist, using LNR Traversal. It then returns the arraylist as a string
+     * @return the contents of the arraylist
+     */
     public static String printTree() {
         ArrayList<String> list;
         MorseCodeTree<String> tree = new MorseCodeTree<>();
